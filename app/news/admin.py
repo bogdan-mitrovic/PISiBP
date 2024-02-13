1eff59268a990425d579c9ac4fb30ae6a1498def
n-markovic <markovic.nemanja001@gmail.com>
2024-02-14 00:31:05 +0100

    Azurirani integralni testovi za unapredjenu verziju aplikacije
    Azurirani integralni testovi za unapredjenu verziju aplikacije

--- app/news/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Category, Comment, News
-
-admin.site.register(Category)
-admin.site.register(News)
-admin.site.register(Comment)
-
